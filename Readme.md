2dabb71ed25f90c0e724073efe75e12a3aaf2743
GroverTB <grover.jtb@gmail.com>
2020-11-03 23:29:52 -0500

    [ UPDATE ] support windows

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Example RN - Android and Windows
---
+++ windows/exampleWindows/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::exampleWindows::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::exampleWindows::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
